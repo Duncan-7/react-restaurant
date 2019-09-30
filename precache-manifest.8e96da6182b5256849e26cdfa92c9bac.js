409bb27471961ded43ba4af75f2a4956aa2377e0
Duncan <duncanmorrison2001@yahoo.com>
2019-10-01 00:06:37 +0100

    Updates

---
+++ precache-manifest.8e96da6182b5256849e26cdfa92c9bac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117d4c1eeec6cf3284d50359134d05b4",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "b4cd81c0705d8f55732d",
+    "url": "/react-restaurant/static/css/main.3472113a.chunk.css"
+  },
+  {
+    "revision": "647c6c8665560a5b2f80",
+    "url": "/react-restaurant/static/js/2.e22928dd.chunk.js"
+  },
+  {
+    "revision": "b4cd81c0705d8f55732d",
+    "url": "/react-restaurant/static/js/main.dfa56bd7.chunk.js"
+  },
+  {
+    "revision": "31d52556b8ed496da869",
+    "url": "/react-restaurant/static/js/runtime-main.cd3abad4.js"
+  },
+  {
+    "revision": "0e78a5e868cb48d232a28218b7e1210f",
+    "url": "/react-restaurant/static/media/background.0e78a5e8.jpg"
+  },
+  {
+    "revision": "866694b7df07d12cdf3fcadcef22e1f0",
+    "url": "/react-restaurant/static/media/burger.866694b7.jpg"
+  },
+  {
+    "revision": "921260db804ec3e7d51d2ef20d93375a",
+    "url": "/react-restaurant/static/media/oysters.921260db.jpg"
+  },
+  {
+    "revision": "0f244325f1ea10bfc5c64f371a33b253",
+    "url": "/react-restaurant/static/media/pie.0f244325.jpg"
+  }
+]);
